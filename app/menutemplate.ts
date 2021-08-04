b68cb711392377309448fc4409035cb178dff6ba
Tevor Thalacker <hello@trevorthalacker.com>
2021-08-04 15:41:08 -0700

    Stuff works without errors

---
+++ app/menutemplate.ts
@@ -0,0 +1,105 @@
+'use strict';
+/*------------------------------------------------------------------------------
+ *  Copyright (c) 2019 Sagar Gurtu
+ *  Licensed under the MIT License.
+ *  See License in the project root for license information.
+ *----------------------------------------------------------------------------*/
+
+import { app, shell } from 'electron';
+
+const isMac = process.platform === 'darwin';
+
+export const menuTemplate: Array<any> = [
+    // { role: 'appMenu' }
+    ...(isMac
+        ? [
+              {
+                  label: app.name,
+                  submenu: [
+                      { role: 'about' },
+                      { type: 'separator' },
+                      { role: 'services' },
+                      { type: 'separator' },
+                      { role: 'hide' },
+                      { role: 'hideothers' },
+                      { role: 'unhide' },
+                      { type: 'separator' },
+                      { role: 'quit' },
+                  ],
+              },
+          ]
+        : []),
+    // { role: 'fileMenu' }
+    {
+        label: 'File',
+        submenu: [
+            { label: 'Open...', id: 'file-open', accelerator: 'CmdOrCtrl+O' },
+            {
+                label: 'Print',
+                id: 'file-print',
+                accelerator: 'CmdOrCtrl+P',
+                enabled: false,
+            },
+        ],
+    },
+    // { role: 'editMenu' }
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            ...(isMac
+                ? [
+                      { role: 'pasteAndMatchStyle' },
+                      { role: 'delete' },
+                      { role: 'selectAll' },
+                      { type: 'separator' },
+                      {
+                          label: 'Speech',
+                          submenu: [
+                              { role: 'startspeaking' },
+                              { role: 'stopspeaking' },
+                          ],
+                      },
+                  ]
+                : [
+                      { role: 'delete' },
+                      { type: 'separator' },
+                      { role: 'selectAll' },
+                  ]),
+        ],
+    },
+    // { role: 'viewMenu' }
+    {
+        label: 'View',
+        submenu: [
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' },
+        ],
+    },
+    // { role: 'windowMenu' }
+    {
+        label: 'Window',
+        submenu: [{ role: 'minimize' }, { role: 'maximize' }],
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click: async () => {
+                    await shell.openExternal(
+                        'https://github.com/advaithm/NightPDF#readme'
+                    );
+                },
+            },
+        ],
+    },
+];
