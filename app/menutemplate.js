b6ca000492ae611b086c4021cd22166e3bfef676
Luna D Dragon <advaith.madhukar@gmail.com>
2022-11-08 13:57:51 +0530

    rework menu template to fix weird stuff and better macos support

--- app/menutemplate.js
+++ app/menutemplate.js
@@ -1,67 +1,95 @@
-'use strict';
+"use strict";
 /*------------------------------------------------------------------------------
  *  Copyright (c) 2019 Sagar Gurtu
  *  Licensed under the MIT License.
  *  See License in the project root for license information.
  *----------------------------------------------------------------------------*/
-const { shell } = require('electron');
+
+const { app, shell } = require('electron');
+
+const isMac = process.platform === 'darwin'
 
 exports.menuTemplate = [
+    // { role: 'appMenu' }
+    ...(isMac ? [{
+      label: app.name,
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []),
     // { role: 'fileMenu' }
     {
-        label: 'File',
-        submenu: [
-            { label: 'Open...', id: 'file-open', accelerator: 'CmdOrCtrl+O' },
-            {
-                label: 'Print',
-                id: 'file-print',
-                accelerator: 'CmdOrCtrl+P',
-                enabled: false,
-            },
-        ],
+      label: 'File',
+      submenu: [
+        {label: 'Open...', id: 'file-open', accelerator: 'CmdOrCtrl+O'},
+        {label: 'Print', id:'file-print', accelerator: 'CmdOrCtrl+P', enabled: false},
+      ]
     },
     // { role: 'editMenu' }
     {
-        label: 'Edit',
-        submenu: [
-            { role: 'undo' },
-            { role: 'redo' },
-            { type: 'separator' },
-            { role: 'cut' },
-            { role: 'copy' },
-            { role: 'paste' },
-        ],
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac ? [
+          { role: 'pasteAndMatchStyle' },
+          { role: 'delete' },
+          { role: 'selectAll' },
+          { type: 'separator' },
+          {
+            label: 'Speech',
+            submenu: [
+              { role: 'startspeaking' },
+              { role: 'stopspeaking' }
+            ]
+          }
+        ] : [
+          { role: 'delete' },
+          { type: 'separator' },
+          { role: 'selectAll' }
+        ])
+      ]
     },
     // { role: 'viewMenu' }
     {
-        label: 'View',
-        submenu: [
-            { role: 'resetzoom' },
-            { role: 'zoomin' },
-            { role: 'zoomout' },
-            { type: 'separator' },
-            { role: 'togglefullscreen' },
-        ],
+      label: 'View',
+      submenu: [
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' }
+      ]
     },
     {
-        role: 'help',
-        submenu: [
-            {
-                label: 'Learn More',
-                click: async () => {
-                    await shell.openExternal(
-                        'https://github.com/Lunarequest/NightPDF#readme'
-                    );
-                },
-            },
-            {
-                label: 'License',
-                click: async () => {
-                    await shell.openExternal(
-                        'https://github.com/Lunarequest/NightPDF/blob/mistress/LICENSE'
-                    );
-                },
-            },
-        ],
-    },
-];
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            await shell.openExternal('https://github.com/Lunarequest/NightPDF')
+          }
+        }
+      ]
+    }
+  ]
