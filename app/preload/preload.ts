13aee5df5f6b3e6c373bb331ea440b2304cc3e52
Luna <luna.dragon@suse.com>
2023-02-15 19:07:14 +0530

    update

--- app/preload/preload.ts
+++ app/preload/preload.ts
@@ -17,31 +17,31 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-'use strict';
-const { contextBridge, ipcRenderer } = require('electron');
+"use strict";
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('api', {
-    getPath: async (filePath: string) => {
-        return await ipcRenderer.invoke('getPath', filePath);
-    },
+contextBridge.exposeInMainWorld("api", {
+	getPath: async (filePath: string) => {
+		return await ipcRenderer.invoke("getPath", filePath);
+	},
 
-    removeAllListeners: (ListenerType: string) => {
-        ipcRenderer.removeAllListeners(ListenerType);
-    },
+	removeAllListeners: (ListenerType: string) => {
+		ipcRenderer.removeAllListeners(ListenerType);
+	},
 
-    openNewPDF: (pdf: string) => {
-        ipcRenderer.send('openNewPDF', pdf);
-    },
-    newWindow: (file: string) => {
-        ipcRenderer.send('newWindow', file);
-    },
-    togglePrinting: (value: boolean) => {
-        ipcRenderer.send('togglePrinting', value);
-    },
-    resizeWindow: (value: string) => {
-        ipcRenderer.send('resizeWindow', value);
-    },
-    on: (eventName: string, callback: any) => {
-        ipcRenderer.on(eventName, callback);
-    },
+	openNewPDF: (pdf: string) => {
+		ipcRenderer.send("openNewPDF", pdf);
+	},
+	newWindow: (file: string) => {
+		ipcRenderer.send("newWindow", file);
+	},
+	togglePrinting: (value: boolean) => {
+		ipcRenderer.send("togglePrinting", value);
+	},
+	resizeWindow: (value: string) => {
+		ipcRenderer.send("resizeWindow", value);
+	},
+	on: (eventName: string, callback: any) => {
+		ipcRenderer.on(eventName, callback);
+	},
 });
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,55 +1,55 @@
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import { uglify } from 'rollup-plugin-uglify';
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { uglify } from "rollup-plugin-uglify";
 
 export default [
-    {
-        input: ['app/main/menutemplate.ts', 'app/main/app.ts'],
-        output: [
-            {
-                dir: './out/main/',
-                format: 'cjs',
-            },
-        ],
-        plugins: [
-            nodeResolve(),
-            typescript(),
-            commonjs({
-                include: './node_modules/**',
-            }),
-            uglify(),
-        ],
-    },
-    {
-        input: 'app/preload/preload.ts',
-        output: [
-            {
-                file: './out/preload/preload.js',
-                format: 'cjs',
-            },
-        ],
-        plugins: [
-            nodeResolve(),
-            typescript({
-                tsconfig: './tsconfig.preload.json',
-            }),
-            uglify(),
-        ],
-    },
-    {
-        input: './app/render/index.ts',
-        output: {
-            file: './out/index/index.js',
-            format: 'iife',
-        },
-        external: ['nouislider'],
-        plugins: [
-            typescript({
-                tsconfig: './tsconfig.app.json',
-                target: 'ES6',
-            }),
-            uglify(),
-        ],
-    },
+	{
+		input: ["app/main/menutemplate.ts", "app/main/app.ts"],
+		output: [
+			{
+				dir: "./out/main/",
+				format: "cjs",
+			},
+		],
+		plugins: [
+			nodeResolve(),
+			typescript(),
+			commonjs({
+				include: "./node_modules/**",
+			}),
+			uglify(),
+		],
+	},
+	{
+		input: "app/preload/preload.ts",
+		output: [
+			{
+				file: "./out/preload/preload.js",
+				format: "cjs",
+			},
+		],
+		plugins: [
+			nodeResolve(),
+			typescript({
+				tsconfig: "./tsconfig.preload.json",
+			}),
+			uglify(),
+		],
+	},
+	{
+		input: "./app/render/index.ts",
+		output: {
+			file: "./out/index/index.js",
+			format: "iife",
+		},
+		external: ["nouislider"],
+		plugins: [
+			typescript({
+				tsconfig: "./tsconfig.app.json",
+				target: "ES6",
+			}),
+			uglify(),
+		],
+	},
 ];
